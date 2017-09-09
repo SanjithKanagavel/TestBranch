b5adc0b3bd47e632b3f55ad7fac6f4802d6196dd
SanjithKanagavel <sanjithkanagavel@gmail.com>
2017-09-10 01:46:27 +0530

    first commit

---
+++ sample app/NSObject+sample.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+sample.h
+//  sample app
+//
+//  Created by Sanjith Kanagavel on 10/09/17.
+//  Copyright © 2017 Sanjith Kanagavel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (sample)
+
+@end
---
+++ sample app/NSObject+sample.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+sample.m
+//  sample app
+//
+//  Created by Sanjith Kanagavel on 10/09/17.
+//  Copyright © 2017 Sanjith Kanagavel. All rights reserved.
+//
+
+#import "NSObject+sample.h"
+
+@implementation NSObject (sample)
+
+@end
